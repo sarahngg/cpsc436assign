691cfd180b8f45bf2a1257279a7c2a123f8b331d
Sarah Ng <sarah.ng26@gmail.com>
2019-05-16 18:53:57 -0700

    Initialized messages with JSON, added name field and random emoji generator

--- myjs.js
+++ myjs.js
@@ -1,25 +1,37 @@
-var chatObj = ["What do you want?", "Regular peal milk tea 30% sugar no ice please.", "Just a moment."];
-//var chatJSON = JSON.parse(chatObj);
+let chatJSON = {"message": ["This is a chat box.", "The \"SEND\" button adds your typed message to this chat and clears the textarea.", "The \"CLEAR\" button clears the text in the textarea.", "The \"CLEAR HISTORY\" clears the chat history in this box, including these useful messages!", "Note: Empty messages will not be added to the chat history"]};
+let chatJSONString = JSON.stringify(chatJSON);
+let chatJSONObj = JSON.parse(chatJSONString);
+let chatObj = [];
+var emojiArr = ["🌚","😬","😳","🙃","🤔"];
 
 function initializeChatHistory() {
-  for (var x in chatObj) {
-    var message = chatObj[x];
+  for (let x in chatJSONObj.message) {
+    let message = chatJSONObj.message[x];
+    chatObj[x] = message;
     document.getElementById("chat-history-list").innerHTML += '<li>' + message + '</li>';
     console.log(message);
   }
 }
 
 function sendMessage() {
-  var msg = document.getElementById("input-message").value;
+  let name = document.getElementById("input-name").value;
+  let msg = document.getElementById("input-message").value;
   //var trimmedMessage = message.trim();
   if(msg != "") {
-    chatObj.push(msg);
-    displayChatMessage(msg);
+    if(name == "") name = randomEmojiPicker();
+    chatObj.push(name + ": " + msg);
+    displayChatMessage(name + ": " + msg);
+    clearTextArea();
   }
-  clearTextArea();
+}
+
+function randomEmojiPicker() {
+  let x = Math.floor((Math.random() * emojiArr.length));
+  return emojiArr[x];
 }
 
 function clearTextArea() {
+  document.getElementById("input-name").value = "";
   document.getElementById("input-message").value = "";
 }
 
